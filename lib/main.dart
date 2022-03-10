95c58c1e4fa55bc3ccf34357218ee1761f9d3e71
UrielRGil <uriel.rgil@alumnos.udg.mx>
2022-03-10 12:55:44 -0600

    Dependencia de one signal ya funciona

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:radiocucei/src/app.dart';
 
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  //Remove this method to stop OneSignal Debugging
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
 
+  OneSignal.shared.setAppId("YOUR_ONESIGNAL_APP_ID");
+
+  // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    print("Accepted permission: $accepted");
+  });
   runApp(Myapp());
 }
