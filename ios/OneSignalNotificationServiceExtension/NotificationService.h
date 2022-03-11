9c926b6658fb26609866e9adc201995d3c61a037
emilio-nuno <emilio.nuno@alumnos.udg.mx>
2022-03-11 11:48:09 -0600

    Agregado soporte de OneSignal para ios

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Servicio Socil on 11/03/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
