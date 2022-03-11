ea55b643a72814c252c05227ce417cea0b0a73a4
UrielRGil <uriel.rgil@alumnos.udg.mx>
2022-03-11 10:38:50 -0600

    OneSignal APP_ID agregado

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:radiocucei/src/app.dart';
 
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  //Remove this method to stop OneSignal Debugging
   OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
 
-  OneSignal.shared.setAppId("YOUR_ONESIGNAL_APP_ID");
+  OneSignal.shared.setAppId("51c63cff-c83b-405e-927b-a9ce3234ec65");
 
   // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
   OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
