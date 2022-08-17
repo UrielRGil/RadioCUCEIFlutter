36db12ce6640456566f2ba30746541d1c4d3cc06
UrielRGil <uriel.rgil@alumnos.udg.mx>
2022-08-17 17:25:58 -0500

    Tabla actualizada 17/08/2022

--- lib/src/componentes/utils.dart
+++ lib/src/componentes/utils.dart
@@ -1,17 +1,18 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void completarFilas(List<dynamic> fila){
+void completarFilas(List<dynamic> fila) {
   int objetosNecesarios = 8 - fila.length;
-  for(int i = 0; i < objetosNecesarios; i++){
+  for (int i = 0; i < objetosNecesarios; i++) {
     fila.add("-");
   }
 }
 
 Future<Horario> fetchHorario() async {
-  final response = await http
-      .get(Uri.parse('https://sheets.googleapis.com/v4/spreadsheets/17MPkO8PwnvsiAdL-U8n56oM-xhPjFPTvPt3xvQO-Keg/values/Sin_Formato!A2:H?key=AIzaSyDDvNYqFdVMbqU6F-HmXXJ1jGCCmLcOS68'));
+  final url = Uri.parse(
+      'https://sheets.googleapis.com/v4/spreadsheets/1D5bPHlNkj7bAlzJVIcL1CR7zo7PnZEAudV7aGvHgMvI/values/Sin_Formato!A2:H?key=AIzaSyDRk_vUZHqUVzdmeKV0CHHCzA2eCtZaBOM');
 
+  final response = await http.get(url);
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
@@ -26,18 +27,14 @@ Future<Horario> fetchHorario() async {
 class Horario {
   final List<dynamic> filas;
 
-  const Horario({
-    required this.filas
-  });
+  const Horario({required this.filas});
 
   factory Horario.fromJson(Map<String, dynamic> json) {
-    return Horario(
-      filas: json['values']
-    );
+    return Horario(filas: json['values']);
   }
 }
 
-Map<String, dynamic> generarDisponibilidad(List<dynamic> horario){
+Map<String, dynamic> generarDisponibilidad(List<dynamic> horario) {
   final Map<int, String> dias = {
     1: 'L',
     2: 'M',
@@ -47,19 +44,18 @@ Map<String, dynamic> generarDisponibilidad(List<dynamic> horario){
     6: 'S',
     7: 'D'
   };
-  
+
   final Map<String, dynamic> disponibilidad = {};
-  horario.forEach((fila){
-    for(int i = 1; i < 8; i++){
-      if(!disponibilidad.containsKey(fila[i])){
+  horario.forEach((fila) {
+    for (int i = 1; i < 8; i++) {
+      if (!disponibilidad.containsKey(fila[i])) {
         disponibilidad[fila[i]] = {};
       }
-      if(!disponibilidad[fila[i]].containsKey(dias[i])){
-          disponibilidad[fila[i]][dias[i]] = [];
-        }
+      if (!disponibilidad[fila[i]].containsKey(dias[i])) {
+        disponibilidad[fila[i]][dias[i]] = [];
+      }
       disponibilidad[fila[i]][dias[i]].add(fila[0]);
     }
-  }
-                 );
+  });
   return disponibilidad;
 }
